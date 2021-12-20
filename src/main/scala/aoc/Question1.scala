d38a04ae866a6e04a49819909bd3ce7260c6012f
Enda111 <30953879+Enda111@users.noreply.github.com>
2021-12-20 18:37:24 -0500

    first question code without imported list

--- src/main/scala/aoc/Question1.scala
+++ src/main/scala/aoc/Question1.scala
@@ -3,5 +3,30 @@ package aoc
 object Question1 extends App {
   println("hello")
 
-  println("hhh")
+  var counter = 0
+  val bounter = 0
+
+  val numbers = List(
+    1, 2, 3, 4, 5, 4, 3, 2, 1, 9, 2, 4, 5, 3, 6, 1, 2, 8, 9, 7, 5, 4, 5, 2, 1
+  )
+
+  /** for { a <- numbers } XXXX
+    *
+    * a = 1 XXXXX
+    * a=2 XXXX
+    * a=5 XXXX
+    * a = 4 XXX
+    */
+
+  for { i <- 0 to numbers.length - 2 } {
+    val first = numbers(i)
+    val second = numbers(i + 1)
+    if (first < second) {
+      counter += 1
+    }
+    println(first + "<->" + second + "<=-=>" + counter)
+
+  }
+
+  println(counter)
 }
