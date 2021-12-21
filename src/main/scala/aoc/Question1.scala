db4ed3155d7c2c9882ccb9430cbd4df0a47d6875
Enda111 <30953879+Enda111@users.noreply.github.com>
2021-12-20 19:11:33 -0500

    first question code with imported list

--- src/main/scala/aoc/Question1.scala
+++ src/main/scala/aoc/Question1.scala
@@ -1,14 +1,18 @@
 package aoc
 
+import scala.io.Source
+
 object Question1 extends App {
   println("hello")
 
   var counter = 0
   val bounter = 0
 
-  val numbers = List(
-    1, 2, 3, 4, 5, 4, 3, 2, 1, 9, 2, 4, 5, 3, 6, 1, 2, 8, 9, 7, 5, 4, 5, 2, 1
-  )
+  //val numbers = List(
+  //  1, 2, 3, 4, 5, 4, 3, 2, 1, 9, 2, 4, 5, 3, 6, 1, 2, 8, 9, 7, 5, 4, 5, 2, 1
+  // )
+  val numbers =
+    Source.fromFile("/Users/enda/IdeaProjects/AOC2021/Q1.txt").getLines.toList
 
   /** for { a <- numbers } XXXX
     *
@@ -19,9 +23,9 @@ object Question1 extends App {
     */
 
   for { i <- 0 to numbers.length - 2 } {
-    val first = numbers(i)
-    val second = numbers(i + 1)
-    if (first < second) {
+    val first = numbers(i).toInt
+    val second = numbers(i + 1).toInt
+    if (second > first) {
       counter += 1
     }
     println(first + "<->" + second + "<=-=>" + counter)
